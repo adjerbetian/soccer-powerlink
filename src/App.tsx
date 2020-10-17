4736c7bd60fc6ecb9ed3b2b84f7dab4c944471e5
Alexandre Djerbetian <alexandre.djerbetian@360learning.com>
2020-10-17 11:38:23 +0200

    Add empty Teams page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import "./App.css";
+import { HomePage, TeamsPage } from "./pages";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 export function App() {
-  return (
-    <div className="App">
-      <h1>Soccer</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <h1>Soccer</h1>
+            <Router>
+                <Switch>
+                    <Route path="/" exact component={HomePage} />
+                    <Route path="/teams" exact component={TeamsPage} />
+                </Switch>
+            </Router>
+        </div>
+    );
 }
---
+++ src/pages/TeamsPage.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export function TeamsPage() {
+    return (
+        <div>
+            <h1>Teams</h1>
+        </div>
+    );
+}
