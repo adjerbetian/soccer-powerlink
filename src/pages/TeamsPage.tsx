b792d79c8d3d63fce10fb12d5a9a65bbd7c6bb0d
Alexandre Djerbetian <alexandre.djerbetian@360learning.com>
2020-10-18 18:32:53 +0200

    Extract TeamList from TeamPage

--- src/pages/TeamsPage.tsx
+++ src/pages/TeamsPage.tsx
@@ -6,51 +6,50 @@ import { TeamSummary } from "../entities";
 import { Loader, TeamCrest } from "../component";
 
 export function TeamsPage() {
-    const [teams, setTeams] = useState<TeamSummary[]>([]);
-    const [loading, setLoading] = useState(true);
-    useEffect(() => {
-        teamService.fetchTeams().then((teams) => {
-            setTeams(teams);
-            setLoading(false);
-        });
-    }, []);
-
-    if (loading) {
-        return (
-            <div>
-                <h1>List of Soccer Teams</h1>
-                <Loader />
-            </div>
-        );
-    }
     return (
         <div>
             <h1>List of Soccer Teams</h1>
-            <table>
-                <thead>
-                    <tr>
-                        <th colSpan={2}>Name</th>
-                        <th>Founded</th>
-                        <th>Address</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {teams.map((team) => (
-                        <tr key={team.id}>
-                            <td>
-                                <TeamCrest team={team} height={15} />
-                            </td>
-                            <td>
-                                <Link to={`/teams/${team.id}`}>
-                                    {team.name}
-                                </Link>
-                            </td>
-                            <td>{team.founded ? team.founded : "-"}</td>
-                            <td>{team.address}</td>
-                        </tr>
-                    ))}
-                </tbody>
-            </table>
+            <TeamList />
         </div>
     );
+}
+function TeamList() {
+    const [teams, setTeams] = useState<TeamSummary[]>([]);
+    const [loading, setLoading] = useState(true);
+    useEffect(() => {
+        fetchTeams();
+    }, []);
+
+    if (loading) return <Loader />;
+    return (
+        <table>
+            <thead>
+                <tr>
+                    <th colSpan={2}>Name</th>
+                    <th>Founded</th>
+                    <th>Address</th>
+                </tr>
+            </thead>
+            <tbody>
+                {teams.map((team) => (
+                    <tr key={team.id}>
+                        <td>
+                            <TeamCrest team={team} height={15} />
+                        </td>
+                        <td>
+                            <Link to={`/teams/${team.id}`}>{team.name}</Link>
+                        </td>
+                        <td>{team.founded ? team.founded : "-"}</td>
+                        <td>{team.address}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    );
+
+    async function fetchTeams() {
+        const teams = await teamService.fetchTeams();
+        setTeams(teams);
+        setLoading(false);
+    }
 }
--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,6 @@ import { config } from "../config";
 
 export const api = {
     async get<T>(uri: string): Promise<T> {
-        console.log("GET", uri);
         return fetch(`${config.api.url}${uri}`, {
             headers: {
                 "X-Auth-Token": config.api.key,
