5fb4e4b40a1367dd9fdbc058499ac6290830f61c
Alexandre Djerbetian <alexandre.djerbetian@360learning.com>
2020-10-18 11:49:58 +0200

    Improve Layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
 import React from "react";
-import "./App.css";
+import "./App.scss";
 import { TeamPage, TeamsPage } from "./pages";
 import {
     BrowserRouter as Router,
     Route,
     Switch,
     Redirect,
+    Link,
 } from "react-router-dom";
 
 export function App() {
     return (
         <div className="App">
-            <h1>Soccer App for Powerlink CRM</h1>
             <Router>
-                <Switch>
-                    <Route path="/" exact>
-                        <Redirect to="/teams" />
-                    </Route>
-                    <Route path="/teams" exact component={TeamsPage} />
-                    <Route path="/teams/:id" component={TeamPage} />
-                </Switch>
+                <Header />
+                <main>
+                    <Switch>
+                        <Route path="/" exact>
+                            <Redirect to="/teams" />
+                        </Route>
+                        <Route path="/teams" exact component={TeamsPage} />
+                        <Route path="/teams/:id" component={TeamPage} />
+                    </Switch>
+                </main>
             </Router>
         </div>
+    );
+}
+function Header() {
+    return (
+        <header>
+            <h2 className="no-mobile left">Alexandre Djerbetian</h2>
+            <div className="no-mobile right">
+                <img
+                    src="https://www.powerlink.co.il/imgs/logofull.png"
+                    alt="PowerLink CRM"
+                />
+            </div>
+            <h1>
+                <Link to="/">Soccer App</Link>
+            </h1>
+        </header>
     );
 }
